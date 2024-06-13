1d9f2974c3244ca41e28def017c8dfeaeb6ba266
Kabilesh-GS <kabileshgs@gmail.com>
2024-06-13 19:36:22 +0530

    Display the value

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,26 @@
 let todoArray = [];
 
+displayTodos();
+
+function displayTodos(){
+  let displayString = '';
+  for(let i =0;i<todoArray.length;i++){
+    let todonames = todoArray[i].Name;
+    let tododates = todoArray[i].Date;
+    let html = `
+      <div>
+        <div>${todonames}</div>
+        <div>${tododates}</div>
+        <button onclick="
+          todoArray.splice(${i},1);
+        ">Remove</button>
+      </div>  
+    `;
+    displayString +=html;
+  }
+  document.querySelector('.displayTodos').innerHTML = `${displayString}`;
+}
+
 function add(){
   let todoValue = document.querySelector('.todoTasks');
   let Name = todoValue.value;
@@ -11,4 +32,8 @@ function add(){
     Date: Date
   });
   console.log(todoArray);
+
+  todoValue.value = '';
+  todoDate.value = '';
+  displayTodos();
 }
